236eb3330b742c4d0ce9af9af5ce44abeeb12912
Niketha Nethaji <nnethaji@ufl.edu>
2024-12-02 23:21:14 +0530

    Update LineChart.js

--- src/app/components/LineChart.js
+++ src/app/components/LineChart.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const AreaChartComponent = ({ data }) => {
-  const states = [...new Set(data.map(item => item.category))]; 
-  const colors = ['#1f77b4', '#ff6347', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#555555', '#bcbd22', '#17becf'];
-
+  const states = [...new Set(data.map(item => item.category))]; // Get unique states
+  const colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
 
   return (
     <ResponsiveContainer width="100%" height={400}>
@@ -18,12 +17,12 @@ const AreaChartComponent = ({ data }) => {
           <Area
             key={state}
             type="monotone"
-            dataKey={state}
+            dataKey="value"
             data={data.filter(item => item.category === state)}
             name={state}
             stroke={colors[index % colors.length]}
             fill={colors[index % colors.length]}
-            fillOpacity={0.3}
+            fillOpacity={0.1}
             
           />
         ))}
