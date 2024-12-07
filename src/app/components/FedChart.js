70569b6998f12ceb348bb6787e220ccd44638d7a
mason-cervi <mason.cervi@ufl.edu>
2024-12-07 13:28:59 -0500

    fed axis label fix

--- src/app/components/FedChart.js
+++ src/app/components/FedChart.js
@@ -65,7 +65,7 @@ const AreaChartComponent = ({ data, fedFundsData }) => {
         <YAxis 
           yAxisId="left" 
           label={{ 
-            value: 'Data Values', 
+            value: '', 
             angle: -90, 
             position: 'insideLeft',
             offset: 10
